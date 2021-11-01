ebb7c5a8800145e17a063b1950f821a8048643c1
Kally <kallyasmedia@gmail.com>
2021-11-01 12:03:30 +0000

    prettier css config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          href="http://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700%7COswald:300,400,500,600,700"
+          rel="stylesheet"
+        />
+        <link href="https://csscdn.pages.dev/css/plugins.min.css" rel="stylesheet"/>
+        <link href="https://csscdn.pages.dev/css/style.css" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
